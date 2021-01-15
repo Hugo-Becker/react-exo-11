1de4a857da676067e7f62cd1c5141e95a6e06821
Hugo <becker.hugo@hotmail.com>
2021-01-15 11:09:54 +0100

    s1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+  state={
+    nombre:1
+  };
+
+  plusUn=()=>{
+    var nbr=this.state.nombre
+
+    this.setState({
+      nombre:nbr+=1
+    })
+  };
+  
+  moinsUn=()=>{
+    var nbr=this.state.nombre
+    this.setState({
+      nombre:nbr-=1
+    })
+  };
+  redify=()=>{
+    let span=document.querySelector('span')
+    if(span.classList.contains('red')){
+      span.classList.remove('red')
+    }else{
+      span.classList.add('red')
+    }
+
+  }
+  render(){
+    return (
+      <div className='content'>
+        <span>{this.state.nombre}</span>
+        <br></br>
+        <button onClick={this.plusUn}>+1</button>
+        <button onClick={this.moinsUn}>-1</button>
+        <button onClick={this.redify}>Redify</button>
+      </div>
+
+    
+      );
+
+  }
+  
 }
 
 export default App;
